b81e0ead292c1dbaa7f9402ab3b552a4f9bc9f4f
prasannadate <datep@rpi.edu>
2019-03-04 19:47:59 -0500

    Added 3.5: Sort Stack

---
+++ Chap3-Stacks-and-Queues/03_stackOfPlates.py
@@ -0,0 +1,151 @@
+class StackNode:
+	""" Linked list node for stack
+
+	Args:
+		val: Value stored at the node
+		next: Pointer to the next node in the stack
+
+	"""
+
+	def __init__(self, val):
+		""" Initializes the node
+
+		Args:
+			val: Value to be stored at the node
+			next: Pointer to next node
+
+		Complexity Analysis:
+			Time Complexity: O(1)
+			Space Complexity: O(1)
+
+		"""
+
+		self.val = val
+		self.next = None
+
+
+
+
+class Stack:
+	""" Linked list as stack
+
+	Args:
+		head: Pointer to the head of the stack
+
+	Methods:
+		push(val): Pushes val onto stack
+		pop(): Pops value from the stack
+		peek(): Returns value at the top of the stack
+		isEmpty(): Checks if stack is empty
+
+	"""
+
+	def __init__(self):
+		""" Initializes the stack
+
+		Complexity Analysis:
+			Time Complexity: O(1)
+			Space Complexity: O(1)
+
+		"""
+
+		self.head = None
+
+
+	def __str__(self):
+		""" Returns string representation of stack
+
+		Returns:
+			String representation of stack
+
+		Complexity Analysis:
+			Time Complexity: O(N)
+			Space Complexity: O(N)
+
+		"""
+
+		stack = []
+		curr = self.head
+		while curr:
+			stack.append(curr.val)
+			curr = curr.next
+		return str(stack)
+
+
+	def push(self, val):
+		""" Pushes val onto the stack
+
+		Args:
+			val: Value to be pushed
+
+		Complexity Analysis:
+			Time Complexity: O(1)
+			Space Complexity: O(1)
+
+		"""
+
+		node = StackNode(val)
+		node.next = self.head
+		self.head = node
+
+
+	def pop(self):
+		""" Pops top of the stack
+
+		Returns:
+			Value in the node at the top of stack
+
+		Raises:
+			ValueError: If stack is empty
+
+		Complexity Analysis:
+			Time Complexity: O(1)
+			Space Complexity: O(1)
+
+		"""
+
+		if self.isEmpty():
+			raise ValueError("Stack is empty")
+
+		node = self.head
+		self.head = self.head.next
+		return node.val
+
+
+	def peek(self):
+		""" Peeks at the topmost node
+
+		Returns:
+			Value in the topmost node
+
+		Raises:
+			ValueError: If stack is empty
+
+		Complexity Analysis:
+			Time Complexity: O(1)
+			Space Complexity: O(1)
+
+		"""
+
+		if self.isEmpty():
+			raise ValueError("Stack is empty")
+
+		return self.head.val
+
+
+	def isEmpty(self):
+		""" Checks if stack is empty
+
+		Returns:
+			True if stack is empty
+
+		Complexity Analysis:
+			Time Complexity: O(1)
+			Space Conmplexity: O(1)
+
+		"""
+
+		return self.head is None
+
+
+
